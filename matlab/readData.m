35b40090b7bfb69c3f635730b8e3cb281ad4464b
zhaoxj <zhaoxj@sjtu.edu.cn>
2017-01-23 19:38:56 +0800

    matlab

---
+++ matlab/readData.m
@@ -0,0 +1,20 @@
+function A=readData(route)
+    order_list=['asc ';'desc'];
+    type_list=['norm';'busy'];
+    auto_list=['0.0';'0.1';'0.3';'0.5';'0.7';'0.9';'1.0'];
+    
+    for i=1:2
+        for j=1:2
+            for k=1:7
+                if(j==1)
+                    order='asc';
+                else
+                    order='desc';
+                end
+                str=['../output/',route,'_',type_list(i,:),'_',order,'_',auto_list(k,:),'.txt'];
+                temp=textread(str);
+                A(i,j,k,:,:)=temp(:,:);                        
+            end
+        end
+    end
+end
