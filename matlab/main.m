6d04bd5390f2daf1183891365f254bf536e1535a
zhaoxj <zhaoxj@sjtu.edu.cn>
2017-01-23 21:06:02 +0800

    zhaoxj

--- matlab/main.m
+++ matlab/main.m
@@ -5,26 +5,23 @@ n=0;
 datav=zeros(1,4);
 datap=zeros(1,4);
 dataq=zeros(1,4);
+temp_datav=zeros(1,4);
+temp_datap=zeros(1,4);
+temp_dataq=zeros(1,4);
 number=zeros(1,4);
 quan=zeros(1,4);
 length=zeros(1,4);
 sump=zeros(1,4);
 sumq=zeros(1,4);
 v=0;p=0;q=0;
-for ii=1:1:10
-for i=1:2
-    A(:,:)=D(2,i,ii,:,:);
-    DATA=[DATA;A];
-end
+for percentage=1:1:7
+for aord=1:2
+DATA(:,:)=D(2,aord,percentage,:,:);
 sza=size(DATA);
-for i=sza(1)/2+1:1:sza(1)
-    DATA(i)=DATA(i-sza(1)/2);
-end
 [B1,B2]=textread('../data/520.txt','%f%f%*[^\n]');
-B1=[B1;B1];B2=[B2;B2];
 szb=size(B1);
 for i=1:1:szb(1)
-    B1(i)=B2(i)-B1(i);
+    B1(i,1)=B2(i,1)-B1(i,1);
 end
 for i=1:1:sza(1)
     if DATA(i,2)==0 && i~=1;
@@ -41,14 +38,26 @@ for i=1:1:sza(1)
     q=q+DATA(i,4);
     p=p+DATA(i,5);
 end
+    ave=v/(n);
+    number(n-1)=number(n-1)+1;%number of sections
+    quan(n-1)=quan(n-1)+ave*B1(DATA(i,1));
+    length(n-1)=length(n-1)+B1(DATA(i,1));
+    sump(n-1)=sump(n-1)+p;
+    sumq(n-1)=sumq(n-1)+q;
 for i=1:1:4
     if length(i)~=0
-        datav(i)=quan(i)/length(i);
+        temp_datav(aord,i)=quan(i)/length(i);
     end
     if number(i)~=0
-        datap(i)=sump(i)/number(i);
-        dataq(i)=sumq(i)/number(i);
+        temp_datap(aord,i)=sump(i)/number(i);
+        temp_dataq(aord,i)=sumq(i)/number(i);
     end
+end
+end
+for i=1:4
+datav=(temp_datav(1,i)+temp_datav(2,i))/2;
+datap=(temp_datap(1,i)+temp_datap(2,i))/2;
+dataq=(temp_dataq(1,i)+temp_dataq(2,i))/2;
 end
 DATAV=[DATAV;datav];
 DATAP=[DATAP;datap];
